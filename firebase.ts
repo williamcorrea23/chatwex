21aa15e3926f0e8445ed26552c841ad06e94ec63
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-07-24 05:50:23 -0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -8,12 +8,12 @@ import { getFunctions } from 'firebase/functions'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  authDomain: "elearning-568mbq.firebaseapp.com",
+  projectId: "elearning-568mbq",
+  storageBucket: "elearning-568mbq.appspot.com",
+  messagingSenderId: "956581108104",
+  appId: "1:956581108104:web:2be9a9b0c5978cd4b3823d",
+  measurementId: "G-WLB4FBXE9R"
 }
 
 // Initialize Firebase

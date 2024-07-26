b82a3ca4a0f3149cc566480873d15ee06e79273e
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-07-26 03:32:48 -0300

    Update firebase-admin.ts

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -4,7 +4,7 @@ var admin = require("firebase-admin");
 
 const  production  ={
     projectId:   process.env.FIREBASE_PROJECT_ID,
-    privateKey:  process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    privateKey:  process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 } ;
 

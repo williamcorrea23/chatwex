632339ace01c811052571a427fb97ed413ba20b2
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-07-26 04:04:40 -0300

    Update firebase-admin.ts

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,26 +1,19 @@
-import admin from 'firebase-admin'
-import { initFirestore } from '@auth/firebase-adapter'
+import admin from 'firebase-admin';
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
 
-let app
+const production = {
+    projectId: "elearning-568mbq",
+    privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCzXNctPngzhq4N\nslEseCRiqw5UvuAmsX0L73VRkT8Q0+LGBiU/w8JUwHKSCOXwQdQyxkl1lyKYdUSw\nKf/4+9SIPmuO0lYq2RJY3qjmT+bZWwR5OF8qh7n7I1ZmaKv9iZnQyEcVXGtM8rVQ\n0npvFP3hmTf+lLKomlzu4NxwMLTLG2Mi3oKJiyUpaokNNv7j/bqwGJ2X1BuF3HpH\nTtlqSuvdk0RFklTCVX+wlHAWUVsr40hBey0d/B0XLph+hLZeWVEYJ5+UTiLGUPQj\nkSSHR7GHztDlJXhHn8NOWemCGedCaMbbQxMW4EjZBnk3bjoFLizv27ufzfIT889e\nysS6R4Q1AgMBAAECggEAKEkyC3d8iBY4AduLX/GcOo0xKte0gPxcmdGvCCMXhzAQ\nP2oh87CEdYTENwjInK4sezsORNstahDs3KSSWdo2YmOixPF5aSZYeJ2KUjCgKTHI\nT3HZ7jOOElpdjaL0/n1pjvts+99GOoJJN14PIeQVIhbH/OZpOnVxajYXTRujf/rG\nhPKsqU3f46GeQYc/XV+Prgj8p79fuyWt60Wob/UB4q+fdejFCqGRqwS4tYmT/uyP\ngsdQs/1kEa+t5AJ4ZBQDvRna21hjbZhAPhE3w6YbYu1DjoH2xg1L17htCIZSMSs4\nBwWVYZajqw8Yb+sJg6vTrEu7uLJeOhXHMp9jRE/gAQKBgQDZI46FH6P6ZWrpmh2u\nAh2gV2namIzKUZ8Puxe/WlXvz4y9OgNH0mafNHoJPZ7cQKeRYHcrSKdmOpVea7PH\nSOsCdNE0ddHrIz6OF0oQZ2g7CSeu4b2tQ9RmtFYiG4+332bNjsnwwXvHci1QcDD2\nLipQjRPundkYPYsWhfjZXLADpQKBgQDTdobHgEJG2AHQ7OGl8yVw7WbI0/Bep+b/\nZpZ7E+Wl8DmwsCVmMyhDU6TpLCK/U9mD9/4aZuT91ZOrQvLxwJ+zoJjIzXRyhjU4\nBmilkf8zjhXEl1geWop+dlK46hlNlkCzapEgig2YvwJH9e09dl2chdoGzxFkMeMi\ncKLKd2pZUQKBgQCF4KVjsNFydgtIAmygN8+6Z4rtazcLGu3+TfVjVrg7hnUYymbx\nZmk9aaaAggyrcfWe7aIr/ryvjBlCQRd0HKJBPnMqbCZEWtOb2gIhQRCDra5Y5fgD\nlxmjRTcCCoo+7S++N3aMyU6e9ue3e6TEdExEo+cRLSOIDZHvDiUTJ1GivQKBgAbR\nXn0WLIzeMAjNamUQVteyP8sf0jMrFK6NXzKurIvSF/oTY9FMuvKYwstlyeUDThMM\nLtwpDx8EANPXv799p2iCOArZXPTIUPTyd/NGpsTBwv/opUWtQ6PaKsadfZ1bx9BB\nElbLPK79EZTCYBjINTJzuljX2kaxuRIGMal3ei1RAoGBAK8zRDJVFbUL1qPusEqD\n4lma1PTgd8JbCIS5qoOQTk6gtKlB7pjsxOr+JGXKHCHOOqSDmBHjI/dc90NGdF4e\nwl+74++KnzjQRVpwJpGbPsTp4CqcPMcHp7AFl+NFPtvD4jKczu3T8I76UQgk32JR\nQsuVNcxDnipe0vjEdBL62uK/\n-----END PRIVATE KEY-----\n",
+    clientEmail: "firebase-adminsdk-ufs3h@elearning-568mbq.iam.gserviceaccount.com"
+};
 
-if (!admin.apps.length) {
-  app = admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
-  })
-}
+admin.initializeApp({
+    credential: admin.credential.cert(production),
+    databaseURL: "https://elearning-568mbq.firebaseio.com"
+});
 
-const adminDB = initFirestore({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  }),
-})
+const adminDB = getFirestore();
+const adminAuth = admin.auth();
 
-const adminAuth = admin.auth(app)
-
-export { adminDB, adminAuth }
+export { adminDB, adminAuth };

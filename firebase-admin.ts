efb124de8e8293133c5bc1a0075d0619d957a5e1
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-07-26 03:36:05 -0300

    Update firebase-admin.ts

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,21 +1,26 @@
+import admin from 'firebase-admin'
+import { initFirestore } from '@auth/firebase-adapter'
 
-var admin = require("firebase-admin");
-	import { initializeApp } from "firebase/app";
+let app
 
-const  production  ={
-    projectId:   process.env.FIREBASE_PROJECT_ID,
-    privateKey:  process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+if (!admin.apps.length) {
+  app = admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    }),
+  })
+}
+
+const adminDB = initFirestore({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-} ;
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  }),
+})
 
-
-
-admin.initializeApp({
-  credential: admin.credential.cert(production)
-});
-
-const adminDB= admin.firestore();
-const adminAuth= admin.auth();
-
+const adminAuth = admin.auth(app)
 
 export { adminDB, adminAuth }

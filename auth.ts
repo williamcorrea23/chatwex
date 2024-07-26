88d92fbf2fe66270ecb283ebce8fff5fa2faaed8
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-07-26 05:48:49 -0300

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -4,39 +4,22 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 
 import { adminAuth, adminDB } from './firebase-admin'
 import { FirestoreAdapter } from '@auth/firebase-adapter'
-
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        username: { label: 'Username', type: 'text' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        if (credentials?.password === process.env.DEMO_USER_PASSWORD) {
-          return {
-            id: 'demo_user_id',
-            name: 'John Doe',
-            email: 'johndoe@example.com',
-            image: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=72&auto=format&fit=crop&q=60',
-          }
-        }
-        return null
-      },
-    }),
   ],
   adapter: FirestoreAdapter(adminDB),
   pages: {
-    signIn: '/auth/signin',  // Customize the sign-in page URL if needed
+    signIn: '/auth/signin', // Página de login customizada se necessário
   },
   session: {
-    strategy: 'database',  // Use database session strategy
+    strategy: 'jwt',
   },
 }
 
-export default NextAuth(authOptions)
+export async function handler(req, res) {
+  return NextAuth(req, res, authOptions)
+}

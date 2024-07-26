90dcfcafb4562d9253dfa3bc36f20fa81be790bd
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-07-26 06:01:15 -0300

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,10 +1,12 @@
-import { NextAuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-import CredentialsProvider from 'next-auth/providers/credentials'
 
-import { adminAuth, adminDB } from './firebase-admin'
-import { FirestoreAdapter } from '@auth/firebase-adapter'
-export const authOptions = {
+
+import {  adminDB } from './firebase-admin'
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { FirestoreAdapter } from '@auth/firebase-adapter';
+
+
+const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -12,11 +14,9 @@ export const authOptions = {
     }),
   ],
   adapter: FirestoreAdapter(adminDB),
-  pages: {
-    signIn: '/auth/signin', // Página de login customizada se necessário
-  },
   session: {
-    strategy: 'jwt',
+    strategy: 'database',
   },
-}
+};
 
+export default (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, authOptions);
